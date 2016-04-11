d8b1e1b3e23aca4ab6b10eb0839e936318202d16
gvenezia14 <gvenezia@wustl.edu>
2016-04-11 18:58:05 -0500

    k

---
+++ 02_files/scripts.js
@@ -0,0 +1,4 @@
+alert("Help me fix this program!");
+alert("Can you get this message to appear?");
+document.write("<h2>Javascript is really fun</h2>");
+document.write("I'm doing stuff");
---
+++ 04_vars/scripts.js
@@ -0,0 +1,11 @@
+
+
+var heading = "";
+var content = "";
+heading += "this heading";
+content += "<h1>" + heading + "</h1>";
+content += "<p>the first text area.</p>";
+
+document.write(content);
+
+console.log(content);
